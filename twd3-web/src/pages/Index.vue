7dcdf56306e4d976ddcff9fe15e653ecf79c74bb
Atria <atriasft@gmail.com>
2020-11-08 05:53:28 +0900

    edit scss etc.

--- twd3-web/src/pages/Index.vue
+++ twd3-web/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <div class="fit row wrap justify-center items-center content-center" background="#3B3B3B">
+    <div class="fit row wrap justify-center items-center content-center" style=" background:#3B3B3B;">
       <div class="col-auto">
       <img
       width="100%"
@@ -10,16 +10,16 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-4">
-        <h1>TwD3</h1>
+      <div class="col-md-4">
+        <h1>TwD3とは</h1>
         <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
       </div>
-      <div class="col-4">
-        <h1>TwD3</h1>
+      <div class="col-md-4">
+        <h1>できること</h1>
         <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
       </div>
-      <div class="col-4">
-        <h1>TwD3</h1>
+      <div class="col-md-4">
+        <h1>利用料金</h1>
         <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
       </div>
     </div>
