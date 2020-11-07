783355bf87a2ddeff5ef48111a48abf611e73f4d
Atria <atriasft@gmail.com>
2020-11-08 06:24:03 +0900

    add timeline-sumple.

--- twd3-web/src/pages/Index.vue
+++ twd3-web/src/pages/Index.vue
@@ -11,18 +11,96 @@
     </div>
     <div class="row">
       <div class="col-md-4">
-        <h1>TwD3とは</h1>
+        <h3>TwD3とは</h3>
         <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
       </div>
       <div class="col-md-4">
-        <h1>できること</h1>
-        <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
+        <h3>できること</h3>
+        <p></p>
       </div>
       <div class="col-md-4">
-        <h1>利用料金</h1>
-        <p>TwD3はTweetDeckの多重起動を検知するGoogleChrome拡張機能です。</p>
+        <h3>利用料金</h3>
+        <p>TwD3は完全無料で提供しています。</p>
       </div>
     </div>
+
+    <div class="q-px-lg q-pb-md">
+    <q-timeline color="secondary">
+      <q-timeline-entry heading tag="h4">
+        更新履歴
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Version 1.2.2"
+        subtitle="2020年10月16日"
+      >
+        <div>
+          他言語(En)対応のアップデート
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Version 1.2.1"
+        subtitle="2020年9月19日"
+      >
+        <div>
+          This time there is one tiny new feature and one bug fix 👍
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry heading tag="h4">
+        v1.1.x
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        color="orange"
+        icon="done_all"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+  </div>
   </q-page>
 </template>
 
